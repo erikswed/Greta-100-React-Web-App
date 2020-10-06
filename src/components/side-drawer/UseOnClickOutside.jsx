5299ee41337649b8d956b6e4bb61c36abe22d698
Hans Erik <erikswed@hotmail.com>
2020-10-06 20:56:46 +0200

    React-Bootstrap Navbar addition

---
+++ src/components/side-drawer/UseOnClickOutside.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+
+export default function useOnClickOutside(ref, handler, exclude) {
+	useEffect(() => {
+		const listener = event => {
+			// Do nothing if clicking ref's element or descendent elements
+			if (!ref.current || ref.current.contains(event.target)) {
+				return;
+			}
+			// do not react on "exclude". Typically the exclude also react so we don't want want double reaction
+			if (event.target.className === exclude) {
+				return;
+			}
+			handler(event);
+		};
+
+		document.addEventListener('mousedown', listener);
+		document.addEventListener('touchstart', listener);
+
+		return () => {
+			document.removeEventListener('mousedown', listener);
+			document.removeEventListener('touchstart', listener);
+		};
+	}, [ref, handler, exclude]);
+}
